1d9a3dbdb4f7056a54910fe76e74872b7900c73e
Chris <TeddiO@users.noreply.github.com>
2024-04-27 21:48:30 +0100

    Add watermark to bottom of page (#55)

---
+++ src/lib/components/footer-watermark.svelte
@@ -0,0 +1,27 @@
+<footer>
+	<div id="watermark-container">
+		<a id="watermark" href="https://bbservers.co.uk" target="_blank" rel="noopener noreferrer"
+			>bbservers.co.uk</a
+		>
+	</div>
+</footer>
+
+<style>
+	#watermark-container {
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin-top: 1em;
+	}
+
+	#watermark {
+		font-size: 10px;
+		color: #f5f5f5;
+		text-decoration: none;
+		margin: 5px;
+	}
+</style>
